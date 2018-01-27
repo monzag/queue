eaedd25379c163fc8446c5428e43af78594cc2f8
monzag <zaganiacz.m@gmail.com>
2018-01-28 00:09:50 +0100

    Add priority field

--- src/main/java/com/monzag/Node.java
+++ src/main/java/com/monzag/Node.java
@@ -4,10 +4,18 @@ public class Node {
 
     private String value;
     private Node next;
+    private Integer priority;
 
     public Node(String value) {
         this.value = value;
         this.next = null;
+        this.priority = null;
+    }
+
+    public Node(String value, Integer priority) {
+        this.value = value;
+        this.next = null;
+        this.priority = priority;
     }
 
     public String getValue() {
@@ -24,5 +32,13 @@ public class Node {
 
     public void setNext(Node next) {
         this.next = next;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
     }
 }
