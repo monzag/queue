ebed328249d0c64ea56ea2fa5a143a838ef36b52
monzag <zaganiacz.m@gmail.com>
2018-01-27 23:49:23 +0100

    Add enqueue method

--- src/main/java/com/monzag/CustomQueue.java
+++ src/main/java/com/monzag/CustomQueue.java
@@ -2,8 +2,19 @@ package com.monzag;
 
 public class CustomQueue {
 
-    public void enqueue(String value) {
+    private Node first;
+    private Node last;
 
+    public void enqueue(String value) {
+        Node newItem = new Node(value);
+        if (first == null) {
+            first = newItem;
+            last = first;
+            
+        } else {
+            last.setNext(newItem);
+            last = newItem;
+        }
     }
 
     public Node peek() {
