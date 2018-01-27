1d015b0b7455a915ae559b493f5152cc61a649ae
monzag <zaganiacz.m@gmail.com>
2018-01-27 23:25:31 +0100

    Create CustomQueue class

---
+++ src/main/java/com/monzag/CustomQueue.java
@@ -0,0 +1,16 @@
+package com.monzag;
+
+public class CustomQueue {
+
+    public void enqueue(String value) {
+
+    }
+
+    public Node peek() {
+
+    }
+
+    public String dequeue() {
+
+    }
+}
