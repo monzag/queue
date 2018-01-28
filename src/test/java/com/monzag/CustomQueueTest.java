422d0b5813b5e600c439255ad1089f4aba457c12
monzag <zaganiacz.m@gmail.com>
2018-01-28 11:03:20 +0100

    Add createStack before each method

---
+++ src/test/java/com/monzag/CustomQueueTest.java
@@ -0,0 +1,18 @@
+package com.monzag;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomQueueTest {
+
+    private CustomQueue queue;
+
+    @BeforeEach
+    void createStack() {
+        this.queue = new CustomQueue();
+    }
+
+
+
+}
