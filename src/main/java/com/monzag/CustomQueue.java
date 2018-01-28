172253d8c4c011dea0259bcc5280138e01d288ae
monzag <zaganiacz.m@gmail.com>
2018-01-28 10:58:06 +0100

    Add priority alghoritm to enqueue method

--- src/main/java/com/monzag/CustomQueue.java
+++ src/main/java/com/monzag/CustomQueue.java
@@ -5,16 +5,36 @@ public class CustomQueue {
     private Node first;
     private Node last;
 
-    public void enqueue(String value) {
-        Node newItem = new Node(value);
+    public void enqueue(String value, Integer priority) {
+        Node newItem = new Node(value, priority);
         if (first == null) {
             first = newItem;
             last = first;
 
-        } else {
+        } else if (priority == null || priority >= getQueueSize() - 1) {
             last.setNext(newItem);
             last = newItem;
+
+        } else if (priority == 1) {
+            newItem.setNext(first);
+            first = newItem;
+
+        } else {
+            enqueueWithPriorityInTheMiddle(newItem);
         }
+    }
+
+    private void enqueueWithPriorityInTheMiddle(Node newItem) {
+        Node current = first;
+        for (int i = 1; i < newItem.getPriority(); i++) {
+            current = current.getNext();
+            if (current == null) {
+                current = newItem;
+                i = newItem.getPriority();
+            }
+        }
+        newItem.setNext(current.getNext());
+        current.setNext(newItem);
     }
 
     public Node peek() {
