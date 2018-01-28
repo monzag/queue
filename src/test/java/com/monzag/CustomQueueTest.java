b368150e3b0780fb77cd9f5cd7aa90b0a4861cca
monzag <zaganiacz.m@gmail.com>
2018-01-28 11:04:54 +0100

    Add isEmptyTest

--- src/test/java/com/monzag/CustomQueueTest.java
+++ src/test/java/com/monzag/CustomQueueTest.java
@@ -1,6 +1,7 @@
 package com.monzag;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +14,9 @@ class CustomQueueTest {
         this.queue = new CustomQueue();
     }
 
-
+    @Test
+    void isEmptyTest() {
+        assertTrue(queue.isEmpty());
+    }
 
 }
